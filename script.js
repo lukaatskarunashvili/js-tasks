213451613625c5b506deff6b24f67efeb3680a7f
lukatskarunashvili <lukaawkarunashvili0@gmail.com>
2023-11-16 00:33:42 +0400

    end

---
+++ script.js
@@ -0,0 +1,30 @@
+let number = 100;
+if (number < 50 ) { 
+    console.log("ნაკლებია50ზ");
+    
+}
+else if (number > 20) {
+    console.log("მეტია20ზე");
+}
+    
+
+
+else {
+    console.log("შეცდომა");
+}
+// 1.
+let name1 = "მარიამი"
+let result = name1 == "მარიამი" ? "true" : "fasl";
+console.log(result);
+// 3.
+let name2 = "მარიამ"
+switch (name2) {
+    case "მარიამ":
+        console.log("true");
+        
+        break;
+
+    default:
+        console.log("fals");
+        
+}
